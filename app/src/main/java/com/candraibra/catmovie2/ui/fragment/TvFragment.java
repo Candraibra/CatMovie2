a15ffe2b3eb38055395bb3859ee0dfd9e35df0a7
Candraibra <canisanie@gmail.com>
2019-11-08 21:45:46 +0700

    refactor

--- app/src/main/java/com/candraibra/catmovie2/ui/fragment/TvFragment.java
+++ app/src/main/java/com/candraibra/catmovie2/ui/fragment/TvFragment.java
@@ -27,7 +27,7 @@ public class TvFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tv_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_tv, container, false);
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {

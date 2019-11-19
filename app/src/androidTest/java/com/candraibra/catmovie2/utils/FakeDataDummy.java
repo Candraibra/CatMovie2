bc9f4b4e6bdb00f7ca298f553c38449d5e85a3af
Candraibra <canisanie@gmail.com>
2019-11-19 07:34:26 +0700

    done

--- app/src/androidTest/java/com/candraibra/catmovie2/utils/FakeDataDummy.java
+++ app/src/androidTest/java/com/candraibra/catmovie2/utils/FakeDataDummy.java
@@ -1,8 +1,8 @@
 /*
  * *
- *  * Created by Candra Ibra Sanie on 11/19/19 7:33 AM
+ *  * Created by Candra Ibra Sanie on 11/19/19 7:34 AM
  *  * Copyright (c) 2019 . All rights reserved.
- *  * Last modified 11/18/19 4:20 PM
+ *  * Last modified 11/19/19 7:34 AM
  *
  */
 
